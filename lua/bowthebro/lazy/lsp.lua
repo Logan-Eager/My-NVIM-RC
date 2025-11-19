b5d056b5f899132c47d9368889d33b19e25dadab
Logan <lea46@uclive.ac.nz>
2025-11-19 19:26:41 +1300

    initial

---
+++ lua/bowthebro/lazy/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  { "williamboman/mason.nvim", config = true },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "omnisharp",
+        "clangd",
+      },
+      automatic_installation = true,
+
+      handlers = {
+        function(server_name)                     -- default handler
+          require("lspconfig")[server_name].setup({})
+        end,
+
+        omnisharp = function()
+          require("lspconfig").omnisharp.setup({
+            cmd = { "omnisharp", "--languageserver", "--host" },
+          })
+        end,
+
+        clangd = function()
+          require("lspconfig").clangd.setup({
+            cmd = { "clangd", "--background-index" },
+          })
+        end,
+      },
+    },
+  },
+
+  { "neovim/nvim-lspconfig" },
+
+  {
+    "hrsh7th/nvim-cmp",
+  },
+}
